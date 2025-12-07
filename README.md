d340a55e1307c379119c3852a9517585d3ce3033
Gleb Fedotov <glebfedotov@gmail.com>
2025-12-08 02:14:02 +0300

    Final version with README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # postgres_clickhouse
+For usage:
+
+cd postgres_clickhouse
+cd certificates/own/
+
+Edit "conf.tpl" and run:
+./gen_certs.sh
+cd ../..
+dnf install podman
+dnf install podman-compose
+./prepare.sh
+podman compose up -d
--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 rm -rf data/clickhouse/*
+rm -rf data/clickhouse/.[^.]*
 rm -rf data/postgres/*
+rm -rf data/postgres/.[^.]*
 chmod 600 ./certificates/own/postgres.key
 chmod 644 ./certificates/own/postgres.crt
 chmod 644 ./certificates/own/ca.crt
-chown -R 999:999 ./data/postgres 
+chown -R 999:999 ./data/postgres
 chown 999:999 ./certificates/own/postgres.key
 chmod 666 certificates/own/clickhouse.key
